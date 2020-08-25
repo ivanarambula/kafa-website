1da443e47b4926150c7a062628b033b534297e53
Ivan <ivanuriel.arambula@gmail.com>
2020-08-25 14:43:20 -0700

    Added anchors to tel and mail

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -6,8 +6,8 @@
             <slim-column>
                 <section class="text">
                     <h1 class="title">Contacto</h1>
-                    <p><span>Teléfono</span>(646) 135 8240</p>
-                    <p><span>Correo electrónico</span>info@kafa-logistics.com</p>
+                    <p><span>Teléfono</span> <a href="tel:(646) 135 8240">(646) 135 8240</a></p>
+                    <p><span>Correo electrónico</span><a target="_blank" href="mailto:info@kafa-logistics.com">info@kafa-logistics.com</a> </p>
                     <p><span>Dirección</span>Calle 2da y Granada Local #9. Ensenada, Baja California. CP 22830</p>
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3388.789272573247!2d-116.6095194128525!3d31.85794906980314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80d893bd404a0153%3A0xa49701c532dd79c9!2sKafa%20Logistics!5e0!3m2!1sen!2smx!4v1596568762935!5m2!1sen!2smx" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                 </section>
@@ -53,6 +53,9 @@ body {
                     font-size: rem(21px);
                     display: block;
                     margin-bottom: $gutter-small;
+                }
+                a {
+                    color: white;
                 }
             }
             ul {
